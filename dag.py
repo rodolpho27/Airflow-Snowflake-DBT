d35a0af217d3a521bba9e78266bbd425955e9a9c
Rodolpho Rodrigo <rodolphortl@gmail.com>
2024-03-20 10:38:56 -0300

    importação de bibliotecas

--- dag.py
+++ dag.py
@@ -1,1 +1,5 @@
-from datetime import datetime 
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostegresHook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
